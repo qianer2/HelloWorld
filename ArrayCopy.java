2780a5850f4ae14658f2169961f5d75ee99f0887
qianer2 <54831004+qianer2@users.noreply.github.com>
2019-10-04 13:36:44 +0800

    Update ArrayCopy.java

--- ArrayCopy.java
+++ ArrayCopy.java
@@ -7,10 +7,10 @@
 public class ArrayCopy{
 	public static void main(String[] args){
 		char a1[] = {'b','b','b'}; 
-        char a2[] = {'a','a','a'}; 
-        char a3[] = new char[a1.length+a2.length]; 
+                char a2[] = {'a','a','a'}; 
+                char a3[] = new char[a1.length+a2.length]; 
 			System.arraycopy(a1,0,a3,0,a1.length); 
-            System.arraycopy(a2,0,a3,a1.length,a2.length); 
-            System.out.print(a3);
+                        System.arraycopy(a2,0,a3,a1.length,a2.length); 
+                        System.out.print(a3);
 	}
 }
