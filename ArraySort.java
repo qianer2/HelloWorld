ca124805e90c24436e49bc5727a4abb804018c60
qianer2 <54831004+qianer2@users.noreply.github.com>
2019-10-04 13:35:04 +0800

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -4,12 +4,13 @@
 *@author:zhuojiaqian
 *@date:2019.9.25
 */
+import java.util.Arrays;
 public class ArraySort{
     public static void main(String[] args){
-         int[] a = {9, 8, 7, 2, 3, 4, 1, 0, 6, 5};        
-         Arrays.sort(a);
-         for(int arr:a){
-			 System.out.print(arr + " ");
+         int[] a = {8,7,9,67,38,3,329,20,5};        
+         Arrays.sort(a);
+         for(int i = 0;i<a.length;i++){
+			 System.out.print(a[i]+"");
 		 }
       }
 }
